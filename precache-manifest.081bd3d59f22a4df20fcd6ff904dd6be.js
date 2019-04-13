4ad1597c470cb8e430ff9b5b24ebaef0f9a20490
Rodrigo Alfonso <rodri042@gmail.com>
2019-04-13 03:29:26 -0300

    Deploy

---
+++ precache-manifest.081bd3d59f22a4df20fcd6ff904dd6be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "88eee44e1c51855ce630",
+    "url": "/nescore/static/js/runtime~main.cfebd61e.js"
+  },
+  {
+    "revision": "b0525bb7feb458266a35",
+    "url": "/nescore/static/js/main.95879798.chunk.js"
+  },
+  {
+    "revision": "5ef3dc9b019005bcbebe",
+    "url": "/nescore/static/js/2.c47aae8a.chunk.js"
+  },
+  {
+    "revision": "b0525bb7feb458266a35",
+    "url": "/nescore/static/css/main.dbd68e0d.chunk.css"
+  },
+  {
+    "revision": "186ded55806e515d378e06358501d6de",
+    "url": "/nescore/index.html"
+  }
+];
